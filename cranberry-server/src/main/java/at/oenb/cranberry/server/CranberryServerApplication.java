5f85f46c1e3006fed22c902b25ed112b8bb654cd
Paul Pacher <paul.pacher@oenb.at>
2022-09-13 09:46:00 +0200

    initialer commit

---
+++ cranberry-server/src/main/java/at/oenb/cranberry/server/CranberryServerApplication.java
@@ -0,0 +1,14 @@
+package at.oenb.cranberry.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CranberryServerApplication {
+
+    public static void main(String ...args) {
+
+        SpringApplication.run(CranberryServerApplication.class, args);
+    }
+}
